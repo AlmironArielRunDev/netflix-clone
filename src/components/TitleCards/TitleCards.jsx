75ae923b23d12fa00b2454f9e29cc2f11cab363f
AlmironArielRunDev <almironariel2070@gmail.com>
2025-04-26 17:41:23 -0300

    cambiando la api key publica

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -6,35 +6,30 @@ import {Link} from 'react-router-dom'
 const TitleCards = ({ title, category }) => {
   const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYTVjZDg5ZmZlMWYyMGQyZDUxNTg2OWE5MzhhNGVmZSIsIm5iZiI6MTc0MzI2MTQ1MS4xNTgwMDAyLCJzdWIiOiI2N2U4MGYwYjZkZWI4ZGE3MjBkZGE5NjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.AS-J5FQd-a93Mtm3MqV29EMdKC7wkD5JGLUysLtR21Y'
-    }
-  };
 
   const handleWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   };
 
-  useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
-      .then(res => res.json())
-      .then(res => {
-        console.log(res);
-        setApiData(res.results);
-      })
-      .catch(err => console.error('Error fetching data:', err));
-  
-    const currentRef = cardsRef.current;
-    currentRef.addEventListener('wheel', handleWheel);
-  
-    return () => {
-      currentRef.removeEventListener('wheel', handleWheel);
-    };
-  }, [category]);
+  const API_KEY = "2a5cd89ffe1f20d2d515869a938a4efe"; // <- clave pública de la api tmdb
+
+useEffect(() => {
+  fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?api_key=${API_KEY}&language=en-US&page=1`)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+      setApiData(res.results);
+    })
+    .catch(err => console.error('Error fetching data:', err));
+
+  const currentRef = cardsRef.current;
+  currentRef.addEventListener('wheel', handleWheel);
+
+  return () => {
+    currentRef.removeEventListener('wheel', handleWheel);
+  };
+}, [category]);
   
 
   return <div className='title-cards'>
